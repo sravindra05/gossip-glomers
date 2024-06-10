b3045bf1fbd3dd3bc8517db8a2fcc45fe8805d56
the-junior-engineer <54739515+sravindra05@users.noreply.github.com>
2024-06-10 08:46:42 +0100

    Add solution for UID generation

---
+++ uid-gen/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		// Update the message type to return back.
+		body["type"] = "generate_ok"
+		body["id"] = genUid(n)
+
+		// Echo the original message back with the updated message type.
+		return n.Reply(msg, body)
+	})
+
+	if err := n.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func genUid(n *maelstrom.Node) string {
+	// Create an ID of the format <node_id>-<timestamp_in_unix_microseconds>
+	return fmt.Sprintf("%s-%d", n.ID(), time.Now().UnixMicro())
+}
